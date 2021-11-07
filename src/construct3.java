f11f00d51f8d5a4b3101ae739c22704f2f653a1b
Anjali <anjalikumari972001@gmail.com>
2021-11-07 12:56:23 +0530

    welcome

---
+++ src/construct3.java
@@ -0,0 +1,12 @@
+
+
+public class construct3 {
+    construct3(int a)
+    {
+        System.out.println("PARAMETERIZED CONSTRUCTOR");
+    }
+
+    public static void main(String[] args) {
+        construct3 aa = new construct3(12);
+    }
+}
